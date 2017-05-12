c16e9a9260e31eadb4295e206ebf1daf6eac3d0c
sahero <sagarshth@gmail.com>
2017-05-12 20:15:57 +1000

    linked mongodb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
- // database: 'mongodb://sagar:sagar@ds127391.mlab.com:27391/usermanagementapp',//'mongodb://localhost:27017/usermanagement',
-  database: 'mongodb://localhost:27017/usermanagement',
+ database: 'mongodb://sagar:sagar@ds137291.mlab.com:37291/userregistration',
+  //database: 'mongodb://localhost:27017/usermanagement',
   secret: 'yoursecret',
   //database_activity: 'mongodb://localhost:27017/useractivity',  
 }
